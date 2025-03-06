27e288d83a7f1c0033479796ffd9812db8dc983c
Antti Vanhala <antti.vanhala@fiksuruoka.fi>
2025-03-06 10:39:36 +0200

    Initial commit

---
+++ src/js/capacitor-welcome.js
@@ -0,0 +1,78 @@
+import { SplashScreen } from '@capacitor/splash-screen';
+import { InAppBrowser } from '@capgo/inappbrowser'
+
+window.customElements.define(
+  'capacitor-welcome',
+  class extends HTMLElement {
+    constructor() {
+      super();
+
+      SplashScreen.hide();
+
+      const root = this.attachShadow({ mode: 'open' });
+
+      root.innerHTML = `
+    <style>
+      .button {
+        display: inline-block;
+        padding: 10px;
+        background-color: #73B5F6;
+        color: #fff;
+        font-size: 0.9em;
+        border: 0;
+        border-radius: 3px;
+        text-decoration: none;
+        cursor: pointer;
+      }
+    </style>
+    <div>
+      <capacitor-welcome-titlebar>
+        <h1>Capacitor</h1>
+      </capacitor-welcome-titlebar>
+      <main>
+        <p>
+          <button class="button" id="web-button">Open webview</button>
+        </p>
+      </main>
+    </div>
+    `;
+    }
+
+    connectedCallback() {
+      const self = this;
+
+      self.shadowRoot.querySelector('#web-button').addEventListener('click', function (e) {
+        InAppBrowser.openWebView({ url: "https://app.urlgeni.us/deeplink-tester" });
+      });
+    }
+  },
+);
+
+window.customElements.define(
+  'capacitor-welcome-titlebar',
+  class extends HTMLElement {
+    constructor() {
+      super();
+      const root = this.attachShadow({ mode: 'open' });
+      root.innerHTML = `
+    <style>
+      :host {
+        position: relative;
+        display: block;
+        padding: 15px 15px 15px 15px;
+        text-align: center;
+        background-color: #73B5F6;
+      }
+      ::slotted(h1) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+        font-size: 0.9em;
+        font-weight: 600;
+        color: #fff;
+      }
+    </style>
+    <slot></slot>
+    `;
+    }
+  },
+);
